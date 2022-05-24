ae5fbe19f0fea3c778ce5a9184c4c349150fe672
DiHolanda <diegopessoal@protonmail.com>
2022-05-24 15:42:39 -0300

    Criada a query que cria a tabela de posts e em seguida o comando para executar - Script executa sem intervenção do usuário, por isso não precisa prevenir de sql injection

--- create_post_tables.php
+++ create_post_tables.php
@@ -1,3 +1,15 @@
 <?php
 
 $conn = require __DIR__.'/utils/connection.php';
+
+$sql = '
+	CREATE TABLE posts(
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		title VARCHAR(50) NOT NULL,
+		body TEXT NOT NULL
+		)
+';
+
+if(!$conn->query($sql)){
+	die('erro: tabela já existe');
+}
