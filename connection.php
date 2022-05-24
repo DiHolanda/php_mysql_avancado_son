d79428fbcba2f90e7cef763c5016243e08c0eda9
DiHolanda <diegopessoal@protonmail.com>
2022-05-24 15:27:15 -0300

    Verificando se retorna erro na conexão - E retorna a variável da conexão feita pelo script -- é uma boa prática

--- connection.php
+++ connection.php
@@ -2,3 +2,9 @@
 
 $conn = new mysqli('localhost', 'root', 'R00t@senhachata', 'php_mysql_avancando');
 
+if($conn->connect_errno){
+	die('Falha de conexão no banco de dados: ' . $conn->connect_errno);
+}
+
+// boa prática para deixar claro que este script retorna uma conexão
+return $conn;
