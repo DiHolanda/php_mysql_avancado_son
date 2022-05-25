50b1998049472232fa6289ebc956ebb73736d52c
DiHolanda <diegopessoal@protonmail.com>
2022-05-25 16:10:47 -0300

    Demonstração de busca por relevância (quando o termo buscado aparece em mais de uma coluna predeterminada)

--- search_fulltext.php
+++ search_fulltext.php
@@ -8,7 +8,7 @@ $term = $argv[1] ?? null;
 //concatena o operador que permite buscar conteúdo que tenha algo a mais escrito além do termo de busca
 $term = '%' . $term . '%';
 
-$stmt = $conn->prepare('SELECT * FROM posts WHERE title LIKE ?;');
+$stmt = $conn->prepare('SELECT *, MATCH(title, body) AGAINST(? IN BOOLEAN MODE) as score FROM posts ORDER BY score DESC;');
 
 $stmt->bind_param('s', $term);
 
@@ -22,5 +22,6 @@ foreach($posts as $post){
 
 	// PHP_EOL adiciona uma quebra de linha
 	echo $post['title'] . PHP_EOL;
-	echo $post['body'] . PHP_EOL . PHP_EOL;
+	echo $post['body'] . PHP_EOL;
+	echo $post['score'] . PHP_EOL . PHP_EOL;
 }
