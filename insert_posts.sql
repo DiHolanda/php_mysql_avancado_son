557920ec04780e1e06e573eb3b5c4057618b82b0
DiHolanda <diegopessoal@protonmail.com>
2022-05-25 14:16:17 -0300

    Criado arquivo com query de inserção

---
+++ insert_posts.sql
@@ -0,0 +1,4 @@
+INSERT INTO posts (tittle, body) VALUES 
+("Laravel framework", "O Laravel é muito utilizado hoje em dia"),
+("Cake PHP", "Framework de desenvolvimento rápido"),
+("Slim frameqork", "Micro framework, podemos utilizar o Eloquent do Laravel nele");
