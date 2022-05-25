8ccd86657529087c808e207dfd188d9ed2d85b92
DiHolanda <diegopessoal@protonmail.com>
2022-05-25 14:23:51 -0300

    Pegando o conteúdo do arquivo sql em uma variável com file_get_contents

--- insert_posts.php
+++ insert_posts.php
@@ -5,3 +5,7 @@ $conn = require __DIR__.'/utils/connection.php';
 // elimina todo o conteúdo da tabela
 // comando usado para ambiente de testes
 $conn->query('TRUNCATE posts');
+
+$sql = file_get_contents(__DIR__.'/insert_posts.sql');
+
+var_dump($sql);
