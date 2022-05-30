17b560368c89a3d12edbf91ca008ce5ef9266b94
DiHolanda <diegopessoal@protonmail.com>
2022-05-30 14:54:34 -0300

    Demonstração de seleção de registros 1-n e a de belongs_to

--- search_posts_and_comments.php
+++ search_posts_and_comments.php
@@ -2,9 +2,19 @@
 
 $conn = require __DIR__.'/utils/connection.php';
 
+// busca um registro de tabela e os relacionados em outra tabela
 $one_to_many = 'SELECT * FROM posts LEFT JOIN comments ON posts.id = comments.post_id WHERE posts.id = 1';
 
-$result = $conn->query($one_to_many);
+// busca um registro de tabela e apenas o relacionado em outra tabela
+$one_to_one = 'SELECT * FROM posts LEFT JOIN comments ON posts.id = comments.post_id WHERE posts.id = 1 GROUP BY posts.id';
+
+// seleciona os registros se baseando na segunda tabela mencionada (a da direita)
+$belongs_to_1 = 'SELECT * FROM posts RIGHT JOIN comments ON posts.id = comments.post_id WHERE posts.id = 1';
+
+// na query a seguir, primeiro puxa os campos da tabela comments, para então trazer o conteúdo relacionado da tabela posts
+$belongs_to_2 = 'SELECT * FROM comments RIGHT JOIN posts ON comments.post_id = posts.id WHERE comments.post_id = 1 GROUP BY posts.id';
+
+$result = $conn->query($belongs_to_2);
 
 $posts = $result->fetch_all(MYSQLI_ASSOC);
 
